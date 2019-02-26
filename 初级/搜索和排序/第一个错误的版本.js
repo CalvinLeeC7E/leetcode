b2b0fc230b37df44a7dacb0055de04a7cbf04ab1
Calvin <421270473@qq.com>
2019-02-26 23:58:44 +0800

    尝试解答

--- 初级/搜索和排序/第一个错误的版本.js
+++ 初级/搜索和排序/第一个错误的版本.js
@@ -4,25 +4,58 @@ var solution = function (isBadVersion) {
    * @return {integer} The first bad version
    */
   return function (n) {
-    let preCheck = Math.floor(n / 2) + 1;
-    let nextCheck = [isBadVersion(preCheck) ? 1 : n]
-    let check = undefined
-    while (check = nextCheck.pop()) {
-      console.log(check)
-      if (isBadVersion(check)) {
-        if (check - preCheck === 1) {
-          return check
-        }
+    let currentVal = 1 + Math.floor((n - 1) / 2)
+    let preVal = currentVal
+    let firstBug = null
+    let queue = [isBadVersion(currentVal) ? 1 : n]
+    while (currentVal = queue.pop()) {
+      console.log('currentVal', currentVal)
+      console.log('preVal', preVal)
+      if (isBadVersion(currentVal)) {
+        firstBug = currentVal
+      } else {
+
       }
-      let tmp = preCheck + Math.floor((check - preCheck) / 2)
-      preCheck = check
-      nextCheck.push(tmp)
+      if (currentVal - preVal > 0) {
+        queue.push(Math.floor(preVal + (currentVal - preVal) / 2))
+        preVal = currentVal
+      } else {
+        
+      }
     }
+    return firstBug
+  }
+  /*
+  return function (n) {
+    let preCheck = Math.floor(n / 2) + 1;
+    let first = null;
+    let nextCheck = [n];
+    if (isBadVersion(preCheck)) {
+      first = preCheck;
+      nextCheck = [1];
+    }
+    let check = undefined;
+    while (check = nextCheck.pop()) {
+      let tmp;
+      if (isBadVersion(check)) {
+        if (check < first) first = check;
+        tmp = preCheck + Math.floor((check - preCheck) / 2);
+      } else {
+        tmp = check + Math.floor((first - check) / 2);
+      }
+      if (tmp === check || tmp === preCheck || tmp === first) {
+        break;
+      }
+      preCheck = check;
+      nextCheck.push(tmp);
+    }
+    return first;
   };
+  */
 };
 
 function test (version) {
-  return [8].includes(version)
+  return [7, 8].includes(version)
 }
 
 let first = solution(test)(8)
