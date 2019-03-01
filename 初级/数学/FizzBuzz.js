2e15f6e3fae3943b37e4abe69419ed8271308ffb
Calvin <421270473@qq.com>
2019-03-01 22:52:15 +0800

    FizzBuzz解答

---
+++ 初级/数学/FizzBuzz.js
@@ -0,0 +1,47 @@
+/**
+ 写一个程序，输出从 1 到 n 数字的字符串表示。
+
+ 1. 如果 n 是3的倍数，输出“Fizz”；
+
+ 2. 如果 n 是5的倍数，输出“Buzz”；
+
+ 3.如果 n 同时是3和5的倍数，输出 “FizzBuzz”。
+
+ 示例：
+ n = 15,
+ 返回:
+ [
+ "1",
+ "2",
+ "Fizz",
+ "4",
+ "Buzz",
+ "Fizz",
+ "7",
+ "8",
+ "Fizz",
+ "Buzz",
+ "11",
+ "Fizz",
+ "13",
+ "14",
+ "FizzBuzz"
+ ]
+ */
+var fizzBuzz = function (n) {
+  const result = [];
+  for (let i = 1; i <= n; i++) {
+    let str = '';
+    if (i % 3 === 0) {
+      str += 'Fizz';
+    }
+    if (i % 5 === 0) {
+      str += 'Buzz';
+    }
+    if (str === '') {
+      str += i;
+    }
+    result.push(str);
+  }
+  return result;
+};
