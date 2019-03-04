86e504ffad6f9e9fc7d63545b91a47fd4a2d0574
Calvin <421270473@qq.com>
2019-03-04 23:04:54 +0800

    《计数质数》题目说明与解答

--- 初级/数学/计数质数.js
+++ 初级/数学/计数质数.js
@@ -1,4 +1,12 @@
 /**
+ 统计所有小于非负整数 n 的质数的数量。
+
+ 示例:
+ 输入: 10
+ 输出: 4
+ 解释: 小于 10 的质数一共有 4 个, 它们是 2, 3, 5, 7 。
+ */
+/**
  * @param {number} n
  * @return {number}
  */
@@ -18,6 +26,3 @@ var countPrimes = function (n) {
   }
   return count;
 };
-
-let res = countPrimes(100)
-console.log(res)
