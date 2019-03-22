59fe0e13d290cf4e29fbdd42b32ee0b01a783dd0
Calvin <421270473@qq.com>
2019-03-22 10:45:45 +0800

    《最长回文子串》解答

--- 中级/数组和字符串/最长回文子串.js
+++ 中级/数组和字符串/最长回文子串.js
@@ -1,8 +1,21 @@
 /**
+ 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+
+ 示例 1：
+ 输入: "babad"
+ 输出: "bab"
+ 注意: "aba" 也是一个有效答案。
+
+ 示例 2：
+ 输入: "cbbd"
+ 输出: "bb"
+ */
+/**
  * @param {string} s
  * @return {string}
  */
 var longestPalindrome = function (s) {
+  // TODO 性能优化
   const len = s.length;
   if (len === 0) return '';
   const haveFindWord = {};
@@ -24,13 +37,14 @@ var longestPalindrome = function (s) {
       continue;
     }
     const wordIndexArr = haveFindWord[item];
-    wordIndexArr.forEach(fItem => {
-      let tmpStr = check(s.slice(fItem, i + 1));
-      if (tmpStr.length > longestStr.length) longestStr = tmpStr;
-    })
+    for (let j = 0; j < wordIndexArr.length; j++) {
+      let tmpStr = check(s.slice(wordIndexArr[j], i + 1));
+      if (tmpStr.length > longestStr.length) {
+        longestStr = tmpStr;
+        break;
+      }
+    }
     wordIndexArr.push(i);
   }
   return longestStr;
 };
-const res = longestPalindrome("abcbbbbbbbbbbbb")
-console.log(res)
